478758e5bdcbe54edce38b37af21024bf65c77ab
zMynx <lior.dux@develeap.com>
2024-03-13 12:22:02 +0200

    minio-op added

--- infrastructue/vault/overlays/prod/bootstrap-script.sh
+++ infrastructue/vault/overlays/prod/bootstrap-script.sh
@@ -1,34 +1,72 @@
-#/bin/sh
+#/bin/bash
+set -ex
 
-KEY_FILE=./unseal.json
-echo "Initializing Vault"
-kubectl exec --stdin=true --tty=true pod/vault-0 -- vault operator init -key-shares=1 -key-threshold=1 -format=json > ./unseal.json
+# *************
+# * Variables *
+# *************
+NUMKEYS=5
+THRESHOLD=3
+KEY_FILE='./unseal.json'
+OUTPUT='/tmp/approle'
+NAMESPACE='vault'
 
-echo "Unsealing Vault-0" 
-kubectl exec --stdin=true --tty=true pod/vault-0 -- vault operator unseal $(jq -r '.unseal_keys_b64[0]' $KEY_FILE) 
-
-echo "Joining Vault nodes"
-kubectl exec --stdin=true --tty=true pod/vault-1 -- vault operator raft join http://vault-0.vault-internal:8200
-kubectl exec --stdin=true --tty=true pod/vault-2 -- vault operator raft join http://vault-0.vault-internal:8200
-
-echo "Unsealing Vault"
-kubectl exec --stdin=true --tty=true pod/vault-1 -- vault operator unseal $(jq -r '.unseal_keys_b64[0]' $KEY_FILE) 
-kubectl exec --stdin=true --tty=true pod/vault-2 -- vault operator unseal $(jq -r '.unseal_keys_b64[0]' $KEY_FILE)
+# *************
+# * Functions *
+# *************
+# echo "Initializing Vault"
+# kubectl --namespace $NAMESPACE exec --stdin=true --tty=true pod/vault-0 -- vault operator init -key-shares=$NUMKEYS -key-threshold=$THRESHOLD -format=json > $KEY_FILE
+# read -p "Press Enter to continue after initializing Vault..."
+#
+# echo "Unsealing Vault-0" 
+# for key in $(seq 0 $((THRESHOLD-1))); do
+#     kubectl --namespace $NAMESPACE exec --stdin=true --tty=true pod/vault-0 -- vault operator unseal $(jq -r ".unseal_keys_b64[$key]" $KEY_FILE)
+# done
+# read -p "Press Enter to continue after unsealing Vault-0..."
+#
+# echo "Joining Vault nodes"
+# for node in $(seq 1 2); do
+#     kubectl --namespace $NAMESPACE exec --stdin=true --tty=true pod/vault-$node -- vault operator raft join http://vault-0.vault-internal:8200
+# done
+#
+# read -p "Press Enter to continue after joining Vault nodes..."
+#
+# echo "Unsealing Vault"
+# for node in $(seq 1 2); do
+#   for key in $(seq 0 $((THRESHOLD-1))); do
+#     kubectl --namespace $NAMESPACE exec --stdin=true --tty=true pod/vault-$node -- vault operator unseal $(jq -r ".unseal_keys_b64[$key]" $KEY_FILE)
+#   done
+# done
+# read -p "Press Enter to continue after unsealing Vault..."
 
 echo "Logging into Vault"
-kubectl exec --stdin=true --tty=true pod/vault-0 -- vault login $(jq -r '.root_token' $KEY_FILE)
+PASSWORD=$(jq -r '.root_token' $KEY_FILE) 
+kubectl --namespace $NAMESPACE exec --stdin=true --tty=true pod/vault-0 -- /bin/sh -c "\
+  vault login $PASSWORD; \
+  vault secrets enable -version=2 -path=secret kv; \
+  cat <<EOF | vault policy write readwrite -;
+  path \"secret/data/*\" { capabilities = [\"create\", \"read\", \"update\", \"delete\", \"list\"] }
+  EOF
+  cat <<EOF | vault policy write readonly -;
+  path \"secret/data/*\" { capabilities = [\"read\", \"list\"] }
+  EOF
+  vault auth enable approle; \
+  vault write auth/approle/role/external-secrets-operator token_policies=\"readonly\"; \
+  vault read auth/approle/role/external-secrets-operator/role-id -format=yaml > $OUTPUT; \
+  echo "---" >> $OUTPUT; \
+  vault write -force auth/approle/role/external-secrets-operator/secret-id -format=yaml >> $OUTPUT;
+"
+read -p "Press Enter to continue fetching AppRole..."
 
-echo "Enabling Vault secrets - KV v2"
-vault secrets enable -version=2 -path=secret kv
+echo "Fetching AppRole"
+kubectl --namespace $NAMESPACE cp vault-0:/$OUTPUT $OUTPUT -c vault
+echo "AppRole fetched - $OUTPUT"
+read -p "Press Enter to continue creating Secret..."
 
-POLICY_DIR=$(pwd)
-echo "Creating Vault policies"
-vault policy write readwrite /etc/vault/policy/read-write-secrets.hcl
-vault policy write readonly /etc/vault/policy/read-only-secrets.hcl
+echo "Update ClusterSecretStore"
+yq -i ".spec.provider.vault.auth.appRole.roleId = $(yq 'select(documentindex==1) | .data.secret_id' $OUTPUT)" ./eso-clustersecretstore.yaml
 
-echo "Creating Vault roles"
-vault auth enable approle
-vault write auth/approle/role/external-secrets-operator token_policies="readonly"
-vault read auth/approle/role/external-secrets-operator/role-id 
-vault write -force auth/approle/role/external-secrets-operator/secret-id
+echo "Creating Secret"
+kubectl --namespace $NAMESPACE create secret generic vault-secret \
+  --from-literal=secret-id=$(yq 'select(documentindex==1) | .data.secret_id' $OUTPUT)
+echo "Secret created - vault-secret"
 
